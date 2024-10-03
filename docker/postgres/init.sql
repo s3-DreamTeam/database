641f715766345076a0c44efb5816324990b3904e
ornitagou <ornitagou@gmail.com>
2024-10-03 19:48:28 -0400

    First part of view creation

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -15,13 +15,15 @@ CREATE TABLE inventaire(
 CREATE TABLE produit(
    produit_id SERIAL,
    quantite INTEGER,
-   prix_achat NUMERIC(15,2)  ,
+   prix_achat INTEGER,
+   image_produit VARCHAR(50) ,
+   nom_produit VARCHAR(50) ,
    PRIMARY KEY(produit_id)
 );
 
 CREATE TABLE achat(
    achat_id SERIAL,
-   achat_produit_id INTEGER,
+   produit VARCHAR(50) ,
    PRIMARY KEY(achat_id)
 );
 
@@ -35,6 +37,7 @@ CREATE TABLE type_de_machine(
    model VARCHAR(50) ,
    manufacturier VARCHAR(50) ,
    no_serie VARCHAR(50) ,
+   image_type_machine VARCHAR(50) ,
    PRIMARY KEY(type_m_id)
 );
 
@@ -78,17 +81,17 @@ CREATE TABLE machine(
    machine_id SERIAL,
    emplacement VARCHAR(50) ,
    nom VARCHAR(50) ,
+   Image_machine VARCHAR(50) ,
    type_m_id INTEGER NOT NULL,
    remplissage_id INTEGER NOT NULL,
-   achat_id INTEGER NOT NULL,
    PRIMARY KEY(machine_id),
    FOREIGN KEY(type_m_id) REFERENCES type_de_machine(type_m_id),
-   FOREIGN KEY(remplissage_id) REFERENCES remplissage(remplissage_id),
-   FOREIGN KEY(achat_id) REFERENCES achat(achat_id)
+   FOREIGN KEY(remplissage_id) REFERENCES remplissage(remplissage_id)
 );
 
 CREATE TABLE type_de_produit(
    type_p_id SERIAL,
+   image_type_produit VARCHAR(50) ,
    nom VARCHAR(50) ,
    marge INTEGER,
    dimension_id INTEGER NOT NULL,
@@ -100,6 +103,7 @@ CREATE TABLE caracteristique_machine(
    caracteristique_m_id SERIAL,
    nom VARCHAR(50) ,
    description VARCHAR(50) ,
+   type VARCHAR(50) ,
    type_caracteristique_m_id INTEGER NOT NULL,
    PRIMARY KEY(caracteristique_m_id),
    FOREIGN KEY(type_caracteristique_m_id) REFERENCES type_de_caracteristique_machine(type_caracteristique_m_id)
@@ -134,6 +138,14 @@ CREATE TABLE usager_x_achat(
    achat_id INTEGER,
    PRIMARY KEY(usager_id, achat_id),
    FOREIGN KEY(usager_id) REFERENCES usager(usager_id),
+   FOREIGN KEY(achat_id) REFERENCES achat(achat_id)
+);
+
+CREATE TABLE achat_x_produit(
+   machine_id INTEGER,
+   achat_id INTEGER,
+   PRIMARY KEY(machine_id, achat_id),
+   FOREIGN KEY(machine_id) REFERENCES machine(machine_id),
    FOREIGN KEY(achat_id) REFERENCES achat(achat_id)
 );
 
@@ -177,12 +189,20 @@ CREATE TABLE type_machine_x_caracteristique(
    FOREIGN KEY(caracteristique_m_id) REFERENCES caracteristique_machine(caracteristique_m_id)
 );
 
-CREATE TABLE type_produit_x_caracteristique(
+CREATE TABLE type_produit_x_caracterisitque(
    type_p_id INTEGER,
    caracteristique_p_id INTEGER,
    PRIMARY KEY(type_p_id, caracteristique_p_id),
    FOREIGN KEY(type_p_id) REFERENCES type_de_produit(type_p_id),
    FOREIGN KEY(caracteristique_p_id) REFERENCES caracteristique_produit(caracteristique_p_id)
+);
+
+CREATE TABLE usager_x_produit(
+   usager_id INTEGER,
+   produit_id INTEGER,
+   PRIMARY KEY(usager_id, produit_id),
+   FOREIGN KEY(usager_id) REFERENCES usager(usager_id),
+   FOREIGN KEY(produit_id) REFERENCES produit(produit_id)
 );
 
 
@@ -204,22 +224,22 @@ VALUES ('Type de produit 2');
 INSERT INTO projet.caracteristique_machine (nom, description, type_caracteristique_m_id)
 VALUES ('Caractéristique machine 1', 'Description 1', 1);
 
-INSERT INTO projet.type_de_machine (model, manufacturier, no_serie)
-VALUES ('Model 1', 'Manufacturier 1', 'No de série 1');
+INSERT INTO projet.type_de_machine (model, manufacturier, no_serie, image_type_machine)
+VALUES ('Model 1', 'Manufacturier 1', 'No de série 1', 'image');
 
 INSERT INTO projet.dimension (nom, largeur, hauteur, longueur, forme, produit_par_unite, est_emballe)
 VALUES ('machine 1', 1, 1, 1, 'Forme 1', 1, TRUE);
 
-INSERT INTO projet.type_de_produit (nom, marge, dimension_id)
-VALUES ('Type de produit 1', 10, 1);
+INSERT INTO projet.type_de_produit (nom, marge, dimension_id, image_type_produit)
+VALUES ('Type de produit 1', 10, 1, 'image');
 
-INSERT INTO projet.produit (quantite, prix_achat)
-VALUES (1, 1);
+INSERT INTO projet.produit (quantite, prix_achat, image_produit)
+VALUES (1, 1, 'image');
 
 INSERT INTO projet.inventaire (inventaire_id)
 VALUES (1);
 
-INSERT INTO projet.achat (achat_produit_id)
+INSERT INTO projet.achat (produit)
 VALUES ('1');
 
 INSERT INTO projet.remplissage (remplissage_id)
@@ -228,8 +248,8 @@ VALUES (1);
 INSERT INTO projet.caracteristique_produit (nom, type_caracteristique_p_id)
 VALUES ('Caractéristique produit 1', 1);
 
-INSERT INTO projet.machine (emplacement, nom, type_m_id, remplissage_id, achat_id)
-VALUES ('Emplacement 1', 'Nom 1', 1, 1, 1);
+INSERT INTO projet.machine (emplacement, nom, type_m_id, remplissage_id, image_machine)
+VALUES ('Emplacement 1', 'Nom 1', 1, 1, 'image');
 
 INSERT INTO projet.usager (nom, prenom, solde, email, profil_pic, status_id)
 VALUES ('Nom 1', 'Prenom 1', 1, 'test', 'test', 1);
@@ -250,3 +270,33 @@ FROM
   projet.usager_x_machines
     JOIN projet.machine ON machine.machine_id = usager_x_machines.machine_id
     JOIN projet.usager ON usager.usager_id = usager_x_machines.usager_id;
+
+CREATE VIEW machine_inventory_page AS
+SELECT
+    projet.machine.nom,
+    projet.machine.image_machine,
+    projet.usager.usager_id
+FROM
+    projet.usager_x_machines
+        JOIN projet.machine ON machine.machine_id = usager_x_machines.machine_id
+        JOIN projet.usager ON usager.usager_id = usager_x_machines.usager_id;
+
+CREATE VIEW machine_inventory_specific AS
+SELECT
+    projet.machine.*,
+    projet.usager.usager_id
+FROM
+    projet.usager_x_machines
+        JOIN projet.machine ON machine.machine_id = usager_x_machines.machine_id
+        JOIN projet.usager ON usager.usager_id = usager_x_machines.usager_id;
+
+CREATE VIEW product_inventory_page AS
+SELECT
+    projet.produit.image_produit,
+    projet.produit.quantite,
+    projet.produit.nom_produit,
+    projet.usager.usager_id
+FROM
+    projet.usager_x_produit
+        JOIN projet.produit ON produit.produit_id = usager_x_produit.produit_id
+        JOIN projet.usager ON usager.usager_id = usager_x_produit.usager_id;
